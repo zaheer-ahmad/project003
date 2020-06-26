a9412e83b012f724991c35023fac9a6b5650ac57
zaheer-ahmad <zaheer.ahmad110@gmail.com>
2020-06-27 00:24:51 +0400

    Submit new changes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Zia Khan" age={25} isReactDeveloper={true} /></div>,
   document.getElementById('root')
 );
 
